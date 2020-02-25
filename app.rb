bd251fc1f5837f45371666160fd67dfe736f5daf
snehasivakumar23 <sneha.sivakumar@kellogg.northwestern.edu>
2020-02-25 02:53:12 +0000

    push

--- app.rb
+++ app.rb
@@ -14,3 +14,20 @@ after { puts; }                                                             #
 
 events_table = DB.from(:events)
 rsvps_table = DB.from(:rsvps)
+
+get "/" do 
+    puts events_table.all
+    @events = events_table.all
+    view "events"
+end
+
+get "/events/:id" do 
+   @event = events_table.where(id: params[:id]).first
+   @date = events_table.where(date: params[:id]).first
+   view "event"
+end  
+
+get "/events/:id/rsvps/new" do
+     @event = events_table.where(id: params[:id]).first
+     view "new_rsvp"
+end
